e270c15681d2381754666b1ae75ddd3e8bbdff12
Ramill <ramill_ibragimov@icloud.com>
2022-10-04 11:36:37 +0300

    Initial Commit

---
+++ ViewThatFitsInSwiftUI/ViewThatFitsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewThatFitsInSwiftUIApp.swift
+//  ViewThatFitsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 04.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewThatFitsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
